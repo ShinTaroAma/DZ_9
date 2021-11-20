8297ff0d8aa37760be808de272785de8673b246f
Momot Roman <fb.momot.r@gmail.com>
2021-11-20 08:46:43 +0200

    Описал объекты

--- dz_9.py
+++ dz_9.py
@@ -2,7 +2,23 @@ import os
 import time
 import pynput
 
+bullets = [
+    {"x": 5, "y": 6},
+    {"x": 10, "y": 3}
+]
+bricks = [
+    {"x":10, "val": 9},
+ 
+    {"x":12, "val": 9},
+    {"x":14, "val": 9},
+    {"x":16, "val": 9}
+]
 
+
+ship = { "x": 5}
+HEIGHT = 10
+WIDTH = 40
+BULLET_SPEED = 0.6
 
 
 
